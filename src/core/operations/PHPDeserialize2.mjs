b58c0bc23a78357ad85f3e576d6caf460d2464c5
Jamie (Bear) Murphy <git@jam.ie>
2025-05-13 19:17:40 +0100

    Update PHPDeserialize2.mjs

--- src/core/operations/PHPDeserialize2.mjs
+++ src/core/operations/PHPDeserialize2.mjs
@@ -10,7 +10,7 @@ import OperationError from "../errors/OperationError.mjs";
 /**
  * PHP Deserialize operation
  */
-class PHPDeserialize extends Operation {
+class PHPDeserialize2 extends Operation {
 
     /**
      * PHPDeserialize constructor
@@ -18,7 +18,7 @@ class PHPDeserialize extends Operation {
     constructor() {
         super();
 
-        this.name = "PHP Deserialize";
+        this.name = "PHP Deserialize2";
         this.module = "Default";
         this.description = "Deserializes PHP serialized data, outputting keyed arrays as JSON.<br><br>This function does not support <code>object</code> tags.<br><br>Example:<br><code>a:2:{s:1:&quot;a&quot;;i:10;i:0;a:1:{s:2:&quot;ab&quot;;b:1;}}</code><br>becomes<br><code>{&quot;a&quot;: 10,0: {&quot;ab&quot;: true}}</code><br><br><u>Output valid JSON:</u> JSON doesn't support integers as keys, whereas PHP serialization does. Enabling this will cast these integers to strings. This will also escape backslashes.";
         this.infoURL = "http://www.phpinternalsbook.com/classes_objects/serialization.html";
@@ -197,4 +197,4 @@ class PHPDeserialize extends Operation {
 
 }
 
-export default PHPDeserialize;
+export default PHPDeserialize2;
